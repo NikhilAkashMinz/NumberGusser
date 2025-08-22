0da20257cc872773f4df3835e80da04a0b9550b5
NikhilAkashMinz <nikhilofficial4747@gmail.com>
2025-08-22 14:03:38 +0530

    Buttons Done

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,44 @@
-import { View,Text } from "react-native";
+import { View,Text,Pressable } from "react-native";
+import { StyleSheet } from "react-native";
 
 function PrimaryButton({children})
 {
-    return<View>
-        <Text>{children}</Text>
-    </View>
+    function PreesMe()
+    {
+        console.log('Pressed');
+    }
+    return(
+        <View style ={styles.buttonOuterContainer}>
+            <Pressable style= {({pressed}) => pressed ? [styles.buttonInnerContainer,styles.pressed]
+            : styles.buttonInnerContainer} 
+            onPress={PreesMe} 
+            android_ripple={{color: '#rgba(255,255,255,0.2)'}}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+    
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer:{
+        borderRadius: 28,
+        margin:4,
+        overflow:'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor:'#830544ff',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    buttonText:{
+        color:'white',
+        textAlign:'center',
+    },
+    pressed:{
+        opacity:0.75,
+    },
+})
